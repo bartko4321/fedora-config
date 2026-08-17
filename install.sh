99802273cc902dedfa870874bd2f2576f772c9ad
Bartosz Szczeciński <54596452+bartko4321@users.noreply.github.com>
2026-08-17 12:12:46 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -235,10 +235,12 @@ for pkg in "${TO_REMOVE[@]}"; do
 done
 sudo dnf5 autoremove -y || true
 
-rm -rf ~/.local/share/akonadi ~/.local/share/kmail2 ~/.local/share/local-mail
-rm -rf ~/.local/share/contacts ~/.local/share/korganizer ~/.local/share/akregator ~/.local/share/kontact
-rm -rf ~/.config/akonadi* ~/.config/kmail* ~/.config/kontact* ~/.config/korganizer*
-rm -rf ~/.config/kaddressbook* ~/.config/akregator* ~/.config/emailidentities ~/.config/mailtransports
+rm -rf ~/.local/share/akonadi ~/.local/share/kmail2 ~/.local/share/local-mail ~/.local/share/contacts ~/.local/share/korganizer ~/.local/share/akregator ~/.local/share/kontact ~/.local/share/konqueror
+rm -rf ~/.config/akonadi* ~/.config/kmail* ~/.config/kontact* ~/.config/korganizer* ~/.config/kaddressbook* ~/.config/akregator* ~/.config/emailidentities ~/.config/mailtransports
+rm -rf ~/.cache/akonadi* ~/.cache/kmail* ~/.cache/kontact* ~/.cache/korganizer* ~/.cache/kaddressbook* ~/.cache/akregator* ~/.cache/konqueror*
+rm -rf ~/.local/share/{epiphany,decibels,gnome-user-docs,gnome-contacts,gnome-maps,gnome-weather}
+rm -rf ~/.config/{epiphany,decibels,gnome-user-docs,gnome-contacts,gnome-maps,gnome-weather}
+rm -rf ~/.cache/{epiphany,decibels,gnome-user-docs,gnome-contacts,gnome-maps,gnome-weather}
 
 mkdir -p ~/.config
 if [[ -f ~/.config/kwalletrc ]]; then
